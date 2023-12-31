66b5a7243ee0e0d51b165b74e7d8a6cb715ca842
LaibaShykh11 <laibaasheikh2001@gmail.com>
2023-12-31 21:20:14 +0500

    mywork

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import WelcomePage from './components/WelcomePage'
+import AllProducts from './components/AllProducts'
+import { AllProjects } from './components/Projects'
+import { Route, Routes } from 'react-router-dom'
+import Reviews from './components/Reviews'
+import { MyInformationProvider } from './context/PersonalInfoContext'
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MyInformationProvider>
+      <div>
+        <Header />
+        <Routes>
+          <Route path={'/'} element={<WelcomePage />} />
+          <Route path={'/all-projects'} element={<AllProjects />} />
+          <Route path={'/products'} element={<AllProducts />} />
+          <Route path={'/reviews'} element={<Reviews />} />
+        </Routes>
+        <Footer />
+      </div>
+    </MyInformationProvider>
+  )
 }
 
-export default App;
+export default App
